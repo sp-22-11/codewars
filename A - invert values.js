392d3f3e0db2107c261a82c043b1bef4ac61570e
sp-22-11 <sp2022cq@proton.me>
2022-11-20 22:16:30 -0500

    map

---
+++ A - invert values.js
@@ -0,0 +1,11 @@
+https://www.codewars.com/kata/5899dc03bc95b1bf1b0000ad/train/javascript
+
+
+
+const invert = orig =>{
+    let verted = orig.map(x => x*-1)
+    return verted
+}
+
+
+const invert = array => array.map(num => -num);
