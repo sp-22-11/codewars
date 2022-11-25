51e4c4c7e8308d70c14f18d12802e216549424f4
sp-22-11 <sp2022cq@proton.me>
2022-11-25 15:47:32 -0500

    string -> number -> string

---
+++ z - sum the strings.js
@@ -0,0 +1,19 @@
+// https://www.codewars.com/kata/5966e33c4e686b508700002d/train/javascript
+
+
+const sumStr = (a,b) =>
+  String(Number(a) + Number(b))
+
+
+// const sumStr = (a,b) =>
+//     (+a + +b) +'' 
+
+
+// sumStr = (a,b) => String (+a + +b);
+
+
+// sumStr = (a, b) =>
+//    ((parseInt(a) || 0) + (parseInt(b) || 0)).toString();
+
+
+// const sumStr = (a,b) => 1*a+  1*b +""
