38673fae3400e614a136b50b455019e3845b536c
sp-22-11 <sp2022cq@proton.me>
2022-11-27 18:26:24 -0500

    from, push

---
+++ A - count the monkeys.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/56f69d9f9400f508fb000ba7
+
+
+const monkeyCount = n =>
+    Array.from( {length:n}, (_, i) => i+1)
+
+
+// function monkeyCount(n) {
+//     for (var i = 0, arr = []; i < n; arr.push(++i));
+//     return arr;
+// }
+
