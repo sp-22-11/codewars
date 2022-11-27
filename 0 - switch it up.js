0bf7242f3f02f98671e24c4734c4599599c19336
sp-22-11 <sp2022cq@proton.me>
2022-11-26 22:19:10 -0500

    switch, array, ?keys

---
+++ 0 - switch it up.js
@@ -0,0 +1,51 @@
+// https://www.codewars.com/kata/5808dcb8f0ed42ae34000031/train/javascript
+
+switchItUp = num => {
+    switch (num) {
+        case 0: return 'Zero';
+          break;
+        case 1: return 'One';
+          break;
+        case 2: return 'Two';
+          break;
+        case 3: return 'Three';
+          break;
+        case 4: return 'Four';
+          break;
+        case 5: return 'Five';
+          break;
+        case 6: return 'Six';
+          break;
+        case 7: return 'Seven';
+          break;
+        case 8: return 'Eight';
+          break;
+        case 9: return 'Nine';
+          break;
+        
+    }
+  }
+
+
+// switchItUp=n=>["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"][n]
+
+
+
+
+// function switchItUp(number){
+// var a = number;
+// var b = {
+// 0: "Zero",
+// 1: "One",
+// 2: "Two",
+// 3: "Three",
+// 4: "Four",
+// 5: "Five",
+// 6: "Six",
+// 7: "Seven",
+// 8: "Eight",
+// 9: "Nine"
+// };
+// return b[a];
+
+// }
